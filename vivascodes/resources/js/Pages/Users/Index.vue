4b87df0ff690c96d3763643ac72af6eb7e7f9c84
Antonio Vivas <81360494+antoniovivasbc@users.noreply.github.com>
2023-05-26 21:37:46 +0000

    Setting controller and view

---
+++ vivascodes/resources/js/Pages/Users/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Welcome</h1>
+    <p>Hello welcome to your first Inertia app!</p>
+</template>
+
+<script setup>
+import Layout from './Layout'
+import { Head } from '@inertiajs/vue3'
+console.log('oi')
+</script>
